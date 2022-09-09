04f9e4abccb0f4037ecd269dc29ddffd639598dc
kuksabb <abbanekua@gmail.com>
2022-09-09 16:08:46 -0700

    Prints the last number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,7 +5,7 @@
 /**
  * main - assigns a random number to n
  * Executes and prints respective output
- * return: 0
+ * return: 0 if success
 */
 int main(void)
 {
@@ -16,15 +16,15 @@ int main(void)
 		printf("Last digit of %d is %d", n, n % 10);
 		if (n % 10 > 5)
 		{
-			printf("and is greater than 5");
+			printf(" and is greater than 5");
 		}
 		else if (n % 10 == 0)
 		{
-			printf("and is 0");
+			printf(" and is 0");
 		}
-		else if (n % 10 < 6 && n % 10 !=0)
+		else if (n % 10 < 6 && n % 10 != 0)
 		{
-		printf("and is less than 6 and not 0\n");
+		printf(" and is less than 6 and not 0\n");
 		}
 return (0);
 }
