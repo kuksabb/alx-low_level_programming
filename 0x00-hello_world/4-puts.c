8dc367f362d5fe38ed93a746311583af405c52e7
kuksabb <abbanekua@gmail.com>
2022-09-08 17:25:55 -0700

    betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,11 +3,12 @@
 /**
  * main - prints 'Programming is like building a multilingual puzzle'
  *
- * Return 0
+ * Return:0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");       	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
 
 	
