26ab49ebfc8571ae8827b636b33f9ad39bd48656
kuksabb <abbanekua@gmail.com>
2022-09-13 17:22:05 -0700

    lowercae alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *  main - prints alphabets in lowercase
+ *  followed by a newline
+ *  Returns: 0
+*/
+void print_alphabet(void)
+{
+	char ch;
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+return (0)
+}
