addfd76526c5f322072ac7cb436b512ddf443deb
kuksabb <abbanekua@gmail.com>
2022-09-14 16:26:47 -0700

    betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
 /**
  * _islower - checks for lowercase character
  * @c: character to check
- * Returns: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if c is lowercase, 0 otherwise
 */
 int _islower(int c)
 {
