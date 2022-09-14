5008e056ac9a66b436c5c4646e68176ffd4e4f7a
kuksabb <abbanekua@gmail.com>
2022-09-13 17:11:10 -0700

    betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,11 +4,12 @@
  * main - prints _putchar, followed by a new line
  *
  * Return: 0
- * */
+*/
 int main(void)
 {
 	char s[] = "_putchar";
 	int i = 0;
+
 	while (s[i] != '\0')
 	{							_putchar(s[i]);
 	i++;
