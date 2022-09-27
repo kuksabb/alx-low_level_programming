ba5df332a2adf603b10603fcfe6cce8d68fb7078
kuksabb <abbanekua@gmail.com>
2022-09-27 12:56:37 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include <"main.h">
+/**
+ * _strchr - locates a character in a string
+ * @c: character to find
+ * @s: string in which to find character
+ * Return: a pointer to first occurence of c
+ * NULL if c not found
+*/
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
