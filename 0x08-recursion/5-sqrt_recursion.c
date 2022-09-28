b4f576589448056f1944d373633ddcfc709934f7
kuksabb <abbanekua@gmail.com>
2022-09-28 15:53:28 -0700

    sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,19 +1,27 @@
 #include "main.h"
 /**
+ * sqrt_check - checks for the square root of c
+ * @g:guess at sqrt
+ * @c: number to find sqrt of
+ * Return: -1 or sqrt of c
+*/
+int sqrt_check(int g, int c)
+{
+	if (g * g == c)
+		return (g);
+	if (g * g > c)
+		return (-1);
+	return (sqrt_check(g + 1, c));
+}
+
+/**
  * _sqrt_recursion - calculates the natural square root of a number
  * @n: number to find its square root
- * Return: natural square root of n
+ * Return: natural square root of n or -1
 */
 int _sqrt_recursion(int n)
 {
-	int m;
-
-	n = m * m;
-
-	if (m * m != n)
-	{
-		return (-1);
-	}
-	_sqrt_recursion(n, 1, m);
-	return (m);
+	if (n == 0)
+		return (0);
+	return (sqrt_check(1, n));
 }
