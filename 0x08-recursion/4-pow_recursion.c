0039b89e5c6339cfda0cfd867e64fc4bccab27ac
kuksabb <abbanekua@gmail.com>
2022-09-28 14:32:17 -0700

    pow rec

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,8 +11,9 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-	else
+	else if (y == 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
+		return (1);
 	}
+	return (x * _pow_recursion(x, y - 1));
 }
