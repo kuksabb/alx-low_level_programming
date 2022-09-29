5025948207ce8145ad823aae6f5f0e8f2c9cebbc
kuksabb <abbanekua@gmail.com>
2022-09-28 17:33:31 -0700

    1o1

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,9 +10,9 @@
 char test(char *s5, char *s6, char *t5, char *t6)
 {
 	if (*s6 == '*' || (*s5 == '\0' && *s6 == '\0'))
-	return (t5);
+	return (*t5);
 	else if (*s5 != *s6)
-	return (wildcard(s5, t6));
+	return (*wildcard(s5, t6));
 	else
 	return (test(s5 + 1, s6 + 1, t5, t6));
 }
@@ -23,13 +23,13 @@ char test(char *s5, char *s6, char *t5, char *t6)
  * @s4: string to check
  * Return: pointer to spot in s3 that matches s4
 */
-char wildcard(char *s3, char *s4)
+char *wildcard(char *s3, char *s4)
 {
 	if (*s3 != *s4)
 	{
 		if (*s3 == '\0')
 		return (0);
-		return (wildcard(s3 + 1, s4));
+		return (*wildcard(s3 + 1, s4));
 	}
 	return (test(s3 + 1, s4 + 1, s3, s4));
 }
@@ -50,7 +50,7 @@ int wildcmp(char *s1, char *s2)
 	return (wildcmp(s1, s2 + 1));
 	else if (*s2 == '*')
 	{
-		p = wildcard(s1, s2 + 1);
+		p = *wildcard(s1, s2 + 1);
 
 		if (p == 0)
 		return (0);
