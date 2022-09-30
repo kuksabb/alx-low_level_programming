fff3b04a00ef51cecb7c220f4aa6b022e141df6b
kuksabb <abbanekua@gmail.com>
2022-09-30 14:56:46 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,32 +14,27 @@ int _atoi(char *s)
 	len = 0;
 	f = 0;
 	digit = 0;
+
 	while (s[len] != '\0')
-	{
 		len++;
-		while (i < len && f == 0)
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
-			if (s[i] == '-')
-			{
-				++d;
-			}
-			if (s[i] >= '0' && s[i] <= '9')
-			{
-				digit = s[i] - '0';
-				if (d % 2)
-				{
-					digit = -digit;
-				n = n * 10 + digit;
-				f = 1;
-				}
-				if (s[i + 1] < '0' || s[i + 1] > '9')
-					break;
-				f = 0;
-			}
-			i++;
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
 		}
-		if (f == 0)
-			return (0);
+		i++;
 	}
-		return (n);
+	if (f == 0)
+		return (0);
+	return (n);
 }
