2a081955b5b4aa9592360a1ef281305deef990ae
kuksabb <abbanekua@gmail.com>
2022-09-30 17:20:26 -0700

    betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,13 +1,14 @@
-#include <stdio.h>                                             
-/**                                                            
- * main - program that prints the arguments passed to it                                               
- * @argc: count of number of arguments passed                  
- * @argv: list of arguments                                    
- * Return: 0                                                   */                                                             
+#include <stdio.h>
+/**                           
+ * main - program that prints the arguments passed to it
+ * @argc: count of number of arguments passed
+ * @argv: list of arguments
+ * Return: 0
+*/
 int main(int argc, char *argv[])
 {
 	int i;
-	
+
 	for (i = 0; a < argc; i++)
 	{
 		printf("%s\n", argv[i]);
