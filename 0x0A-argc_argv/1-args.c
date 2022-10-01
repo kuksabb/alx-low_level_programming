635695ece5f36c14a8fbb3fc9be0e3f799aa6ca2
kuksabb <abbanekua@gmail.com>
2022-09-30 17:01:38 -0700

    correction of code

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv: list of arguments
  * Return: 0
 */
-int main(int argc, char __attribute__((unsed)) *argv[])
+int main(int argc, char __attribute__((unused)) *argv[])
 {
 	printf("%d\n", argc - 1);
 	return (0);
