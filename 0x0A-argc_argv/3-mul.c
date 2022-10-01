4fbee608c5ba698b9fe20e597045f89d05806194
kuksabb <abbanekua@gmail.com>
2022-09-30 18:02:22 -0700

    betty

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,15 +2,14 @@
 #include <stdio.h>
 /**
  * main - multiplies 2 numbers
- * @i: one of the numbers
- * @a: the other number
- * @result - result of multiplication
+ * @argc: number of arguments
+ * @argv: arguments supplied
  * Return: result or 1 if it doesnt receive 2 arguments
 */
 int main(int argc, char *argv[])
 {
 	int a = 0, b = 0;
-	
+
 	if (argc > 2)
 	{
 		a = atoi(argv[1]);
