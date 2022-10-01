f1da73f31f4e908a0180ed571495cc27ab400d97
kuksabb <abbanekua@gmail.com>
2022-09-30 17:12:24 -0700

    2-args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,10 @@
+#include <stdio.h>                                             
+/**                                                            
+ * main - program that prints the arguments passed to it                                               
+ * @argc: count of number of arguments passed                  
+ * @argv: list of arguments                                    
+ * Return: 0                                                   */                                                             
+int main(int __attribute__((unused)) argc, char *argv[])       {
+	printf("%s\n", argv[]);
+	return (0);
+}
