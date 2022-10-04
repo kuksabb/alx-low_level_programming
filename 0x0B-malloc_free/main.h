b28f3bbc5f366f4ae32ffe441a2059b3f28168a4
kuksabb <abbanekua@gmail.com>
2022-10-04 14:13:54 -0700

    argstostr

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
 
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
