4b69b7268b4685ab4e63ad522f8b0931b9bfa1aa
kuksabb <abbanekua@gmail.com>
2022-10-04 13:21:32 -0700

    str_concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,6 +30,8 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	}
 	for (i = 0; s1[i] != '\0'; i++)
+		strnew[i] = s1[i];
+	for (;s2[count] != '\0'; i++)
 	{
 		strnew[i] = s2[count];
 		count++;
