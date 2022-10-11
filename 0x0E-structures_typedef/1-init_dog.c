820584c8f268d518ae7d32327bc6d206400c013d
kuksabb <abbanekua@gmail.com>
2022-10-10 17:30:54 -0700

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * init_dog - initialize a variable of type struct dog
+ * @d: passing structure to the function
+ * @name: stores a string
+ * @age: stores a string
+ * @owner: stores a string
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner);
+{
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
