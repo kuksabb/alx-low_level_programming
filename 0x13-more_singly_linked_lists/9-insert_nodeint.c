75a3ff7b2b3d67643b40904869ac0c58bda0692d
kuksabb <abbanekua@gmail.com>
2022-10-25 13:53:50 -0700

    betty

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		node = add_nodeint(head, n);
 		return (node);
 	}
-	
 	node = malloc(sizeof(listint_t));
 	if (!node)
 		return (0);
